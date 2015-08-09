62ce014a6907b8f404d041a4fc845d1b8949dc82
Piper Gragg <pgragg@dwight.edu>
2015-08-08 21:28:23 -0400

    Seeds.rb creates new users and items.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+User.delete_all
+Item.delete_all
+
+10.times do 
+  User.create(email: Faker::Internet.email, password: "password")
+end
+
+users = User.all 
+
+100.times {users.sample.items.create(name: Faker::Commerce.product_name)}
+
+
+# create_table "items", force: :cascade do |t|
+#     t.string   "name"
+#     t.integer  "user_id"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#   end
+
+#   add_index "items", ["user_id"], name: "index_items_on_user_id"
+
+#   create_table "users", force: :cascade do |t|
+#     t.string   "email"
+#     t.string   "password_hash"
+#     t.string   "password_salt"
+#     t.datetime "created_at",    null: false
+#     t.datetime "updated_at",    null: false
+#   end
