a0168a9b58c6991d6a53040571360f12c9de5170
Piper Gragg <pgragg@dwight.edu>
2015-08-08 23:54:46 -0400

    AJAX removal of todo list items.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,6 +11,16 @@ class ItemsController < ApplicationController
     redirect_to user_path(@user) 
   end
 
+  def destroy  
+    @user = User.find(params[:user_id])
+    @item = @user.items.find(params[:id])
+    @item.destroy
+    respond_to do |format|
+      format.html
+      format.js   #{ render :layout => false }
+    end
+  end 
+
   private 
 
   def item_params
